df03401f78f56a251c42bf50e82443803ef32f80
Rob Murray <robmurray17@gmail.com>
2014-04-30 20:32:23 +0100

    Refactor out a bit of duplication in tests

--- spec/controller/repos_controller_spec.rb
+++ spec/controller/repos_controller_spec.rb
@@ -5,9 +5,7 @@ describe HazCommitz::ReposController do
 
     context "given a request with repos path" do
 
-        before(:each) do
-            get("/repos")
-        end
+        before { get("/repos") }
 
         it "should return redirect" do
             expect(last_response).to be_redirect
@@ -22,14 +20,11 @@ describe HazCommitz::ReposController do
 
     end
 
-    context "given an html request to with repo owner and repo" do
+    context "given a request with repo owner and name" do
 
-        before(:each) do
+        before do
             HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).returns(repo)
-
             repo.stubs(:latest_commit).returns(commit)
-
-            get("/repos/owner_name/repo_name")
         end
 
         let(:repo) do
@@ -40,171 +35,170 @@ describe HazCommitz::ReposController do
             HazCommitz::Commit.new("sha-12345", "joe bloggs", time_days_ago(7), "i fixed stuff")
         end
 
-        context "given a valid owner name and repo name" do
+        context "with valid repo" do
 
-            it "should request repo info" do
-                HazCommitz::RepositoryService.any_instance.expects(:repo_with_last_commit).with('owner_name', 'repo_name').returns(repo)
-                get("/repos/owner_name/repo_name")
+            context "to html url" do
+
+                before { get("/repos/owner_name/repo_name") }
+
+                it "should request repo info" do
+                    HazCommitz::RepositoryService.any_instance.expects(:repo_with_last_commit).with('owner_name', 'repo_name').returns(repo)
+                    get("/repos/owner_name/repo_name")
+                end
+
+                it "should return valid response" do
+                    expect(last_response).to be_ok
+                end
+
+                it "should render repo view" do
+                    expect(last_response.body).to have_selector("div#repo")
+                end
+
+                it "should display repo owner and name" do
+                    expect(last_response.body).to contain(/owner_name/)
+                    expect(last_response.body).to contain(/repo_name/)
+                end
+
+                it "should display rating" do
+                    expect(last_response.body).to have_selector('span', :content => "#{repo.rating}")
+                end
+
+                it "should display last commit info" do
+                    expect(last_response.body).to contain(/sha-12345/)
+                    expect(last_response.body).to contain(/joe bloggs/)
+                    expect(last_response.body).to contain(/i fixed stuff/)
+                end
+
+                it "should display image with badge" do
+                    expect(last_response.body).to have_xpath("//img[@src='/repos/owner_name/repo_name/badge.svg']")
+                end
+
+                context "with a last commit that has no message" do
+
+                    let(:commit) do
+                        HazCommitz::Commit.new("sha-12345", "joe bloggs", time_days_ago(7))
+                    end
+
+                    it "should not display any message or indication of" do
+                        expect(last_response.body).not_to contain(/Message/)
+                    end
+
+                end
+
             end
 
-            it "should return valid response" do
-                expect(last_response).to be_ok
-            end
+            context "to badge url" do
 
-            it "should render repo view" do
-                expect(last_response.body).to have_selector("div#repo")
-            end
+                before { get("/repos/owner_name/repo_name/badge.svg") }
 
-            it "should display repo owner and name" do
-                expect(last_response.body).to contain(/owner_name/)
-                expect(last_response.body).to contain(/repo_name/)
-            end
+                it "should request repo info" do
+                    HazCommitz::RepositoryService.any_instance.expects(:repo_with_last_commit).with('owner_name', 'repo_name').returns(repo)
+                    get("/repos/owner_name/repo_name/badge.svg")
+                end
 
-            it "should display rating" do
-                expect(last_response.body).to have_selector('span', :content => "#{repo.rating}")
-            end
+                it "should return valid response" do
+                    expect(last_response).to be_ok
+                end
 
-            it "should display last commit info" do
-                expect(last_response.body).to contain(/sha-12345/)
-                expect(last_response.body).to contain(/joe bloggs/)
-                expect(last_response.body).to contain(/i fixed stuff/)
-            end
+                it "should return svg content type" do
+                    expect(last_response.content_type).to include('image/svg+xml')
+                end
 
-            it "should display image with badge" do
-                expect(last_response.body).to have_xpath("//img[@src='/repos/owner_name/repo_name/badge.svg']")
+                it "should return an image"
+
+                it "should set etag in response headers" do
+                    expect(last_response['Etag']).not_to be_nil
+                    expect(last_response['Etag']).not_to be_empty
+                end
+
+                it "should set caching directive" do
+                    expect(last_response['Cache-Control']).to eq('no-cache, no-store, must-revalidate')
+                end
+
             end
 
         end
 
-        context "given a last commit with no message" do
+        context "with a repo that does not exist" do
 
-            let(:commit) do
-                HazCommitz::Commit.new("sha-12345", "joe bloggs", time_days_ago(7))
-            end
-
-            it "should not display any message or indication of" do
-                expect(last_response.body).not_to contain(/Message/)
-            end
-
-        end
-
-        context "given a repo name that does not exist" do
-
-            before(:each) do
+            before do
                 HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::NotFound)
-                get("/repos/owner_name/repo_name")
             end
 
-            it "should return not found status" do
-                expect(last_response.status).to eq(404)
+            context "to html url" do
+
+                before { get("/repos/owner_name/repo_name") }
+
+                it "should return not found status" do
+                    expect(last_response.status).to eq(404)
+                end
+
+            end
+
+            context "to badge url" do
+
+                before { get("/repos/owner_name/repo_name/badge.svg") }
+
+                it "should return not found status" do
+                    expect(last_response.status).to eq(404)
+                end
+
             end
 
         end
 
-        context "given a repo that is unauthorised" do
+        context "with a repo that is unauthorised" do
 
-            before(:each) do
+            before do
                 HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::Unauthorized)
-                get("/repos/owner_name/repo_name")
             end
 
-            it "should return unauthorised status" do
-                expect(last_response.status).to eq(401)
+            context "to html url" do
+
+                before { get("/repos/owner_name/repo_name") }
+
+                it "should return unauthorised status" do
+                    expect(last_response.status).to eq(401)
+                end
+
+            end
+
+            context "to badge url" do
+
+                before { get("/repos/owner_name/repo_name/badge.svg") }
+
+                it "should return unauthorised status" do
+                    expect(last_response.status).to eq(401)
+                end
+
             end
 
         end
 
-        context "given github api rate limit exceeded" do
+        context "when the GitHub API rate limit is exceeded" do
 
-            before(:each) do
+            before do
                 HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::TooManyRequests)
-                get("/repos/owner_name/repo_name/badge.svg")
             end
 
-            it "should return service unavailble status" do
-                expect(last_response.status).to eq(503)
+            context "to html url" do
+
+                before { get("/repos/owner_name/repo_name") }
+
+                it "should return service unavailble status" do
+                    expect(last_response.status).to eq(503)
+                end
+
             end
 
-        end
+            context "to badge url" do
 
-    end
+                before { get("/repos/owner_name/repo_name/badge.svg") }
 
-    context "given a badge request for repo owner and repo" do
+                it "should return service unavailble status" do
+                    expect(last_response.status).to eq(503)
+                end
 
-        before(:each) do
-            HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).returns(repo)
-
-            repo.stubs(:latest_commit).returns(commit)
-
-            get("/repos/owner_name/repo_name/badge.svg")
-        end
-
-        let(:repo) do
-            HazCommitz::GithubRepository.new('owner_name', 'repo_name')
-        end
-
-        let(:commit) do
-            HazCommitz::Commit.new("sha-12345", "joe bloggs", time_days_ago(7), "i fixed stuff")
-        end
-
-        context "given a valid owner name and repo name" do
-
-            it "should return valid response" do
-                expect(last_response).to be_ok
-            end
-
-            it "should return svg content type" do
-                expect(last_response.content_type).to include('image/svg+xml')
-            end
-
-            it "should return image"
-
-            it "should set etag in response headers" do
-                expect(last_response['Etag']).not_to be_nil
-                expect(last_response['Etag']).not_to be_empty
-            end
-
-            it "should set caching directive" do
-                expect(last_response['Cache-Control']).to eq('no-cache, no-store, must-revalidate')
-            end
-
-        end
-
-        context "given a repo name that does not exist" do
-
-            before(:each) do
-                HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::NotFound)
-                get("/repos/owner_name/repo_name/badge.svg")
-            end
-
-            it "should return not found status" do
-                expect(last_response.status).to eq(404)
-            end
-
-        end
-
-        context "given a repo that is unauthorised" do
-
-            before(:each) do
-                HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::Unauthorized)
-                get("/repos/owner_name/repo_name/badge.svg")
-            end
-
-            it "should return unauthorised status" do
-                expect(last_response.status).to eq(401)
-            end
-
-        end
-
-        context "given github api rate limit exceeded" do
-
-            before(:each) do
-                HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::TooManyRequests)
-                get("/repos/owner_name/repo_name/badge.svg")
-            end
-
-            it "should return service unavailble status" do
-                expect(last_response.status).to eq(503)
             end
 
         end
@@ -213,7 +207,7 @@ describe HazCommitz::ReposController do
 
     context "given a get request to add a repository" do
 
-        before(:each) do
+        before do
             get("/repos/new")
         end
 
@@ -231,9 +225,8 @@ describe HazCommitz::ReposController do
 
         context "given a request in valid format" do
 
-            before(:each) do
+            before do
                 HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).returns(repo)
-
                 repo.stubs(:latest_commit).returns(commit)
 
                 post '/repos/new', {"repo_path"=>"name/repo"}
@@ -259,9 +252,9 @@ describe HazCommitz::ReposController do
             end
         end
 
-        context "given an invalid request" do
+        context "with invalid data from form" do
 
-            before(:each) { post '/repos/new', {"repo_path"=>"blah"} }
+            before { post '/repos/new', {"repo_path"=>"blah"} }
 
             it "should redirect to /add" do
                 expect(last_response).to be_redirect
