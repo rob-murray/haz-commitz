7b32b246f555c9d1c256f4078bcfacb1d105f800
Rob Murray <robmurray17@gmail.com>
2014-05-18 10:25:38 +0100

    Drop in service

---
+++ app/services/repository_service.rb
@@ -0,0 +1,25 @@
+
+module HazCommitz
+  class RepositoryService
+    def initialize(github_api)
+      @github_api = github_api
+    end
+
+    def repo(owner, repo)
+      GithubRepository.new(owner, repo)
+    end
+
+    def repo_with_last_commit(owner, repo)
+      repo = repo(owner, repo)
+      repo.latest_commit = commit_builder.latest_commit(repo.path)
+
+      repo
+    end
+
+    private
+
+    def commit_builder
+      LatestCommitBuilder.new(@github_api)
+    end
+  end
+end
