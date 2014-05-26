c889e4d5058f2ac593f7c9cebc639ec2de763dd7
Rob Murray <robmurray17@gmail.com>
2014-05-26 14:15:49 +0100

    Add an adapter around github api

--- lib/github_adapter.rb
+++ lib/github_adapter.rb
@@ -1,22 +1,20 @@
 require 'octokit'
 require 'faraday/http_cache'
 
-module HazCommitz
-  class GithubAdapter
-    def initialize(token)
-      stack = Faraday::RackBuilder.new do |builder|
-        builder.use Faraday::HttpCache
-        builder.use Octokit::Response::RaiseError
-        builder.adapter Faraday.default_adapter
-      end
-      Octokit.middleware = stack
-
-      @client = Octokit::Client.new(access_token: token)
+class GithubAdapter
+  def initialize(token)
+    stack = Faraday::RackBuilder.new do |builder|
+      builder.use Faraday::HttpCache
+      builder.use Octokit::Response::RaiseError
+      builder.adapter Faraday.default_adapter
     end
+    Octokit.middleware = stack
 
-    def list_commits(repo_path, branch)
-      # https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
-      @client.list_commits(repo_path, sha: branch)
-    end
+    @client = Octokit::Client.new(access_token: token)
+  end
+
+  def list_commits(repo_path, branch)
+    # https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
+    @client.list_commits(repo_path, sha: branch)
   end
 end
