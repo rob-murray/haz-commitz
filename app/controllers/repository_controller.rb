598ad3b04166b2499dbf13c0a6714b7774cf7e65
Rob Murray <robmurray17@gmail.com>
2014-05-26 14:20:36 +0100

    Implement spec for Repository controller

--- app/controllers/repository_controller.rb
+++ app/controllers/repository_controller.rb
@@ -1,10 +1,25 @@
 class RepositoryController < ApplicationController
+  rescue_from Exception, with: :something_went_wrong
+  rescue_from Octokit::NotFound, with: :repository_not_found
+  rescue_from Octokit::Unauthorized, with: :repository_not_authorised
+  rescue_from Octokit::TooManyRequests, with: :over_api_limit
+
   def index
     redirect_to root_url
   end
 
   def show
+    @repo = fetch_and_rate_repo(params[:user_id], params[:id])
 
+    respond_to do |format|
+      format.html
+      format.svg do
+        badge = badge(@repo.rating)
+        expires_now
+        fresh_when(@repo.latest_commit.sha)
+        render text: ImageProxy.fetch(badge.display)
+      end
+    end
   end
 
   def new
@@ -18,7 +33,7 @@ class RepositoryController < ApplicationController
       flash[:error] = 'Invalid repository format'
       redirect_to new_repository_path
     else
-      redirect_to "/repos/#{repo.path}"
+      redirect_to repository_path user_id: repo.owner, id: repo.name
     end
   end
 
@@ -26,5 +41,60 @@ class RepositoryController < ApplicationController
 
   def new_repo_params
     params.require(:repository).permit(:path)
+  end
+
+  def repository_not_found
+    render_error :not_found, 'Github Repository not found.'
+  end
+
+  def repository_not_authorised
+    render_error :unauthorized, 'Unauthorised to access Github Repository.'
+  end
+
+  def over_api_limit
+    render_error :service_unavailable, 'Github API rate limited exceeded. Try again soon.'
+  end
+
+  def something_went_wrong
+    render_error :internal_server_error, 'Ooops'
+  end
+
+  def render_error(status, message = '')
+    respond_to do |f|
+      f.html { render plain: message, status: status }
+      f.svg do
+        error_badge = File.join(Rails.public_path, 'error_badge.svg')
+        render text: File.read(error_badge), status: status
+      end
+    end
+  end
+
+  # TODO: move these...
+
+  def fetch_and_rate_repo(owner, repo_name)
+    repo = repo_service.repo_with_last_commit(owner, repo_name)
+    repo.rate_with(repo_rater)
+
+    repo
+  end
+
+  def github_client
+    @client ||= GithubAdapter.new(Rails.application.secrets.github_api_key)
+  end
+
+  def repo_rater
+    TimeBasedRepoRater.new
+  end
+
+  def repo_service
+    @repo_service ||= RepositoryService.new(github_client)
+  end
+
+  def badge(repo_rating)
+    BadgeUrlRatingPresenter.new(repo_rating)
+  end
+
+  def repo_from_path(repo_path)
+    GithubRepository.new_from_path(repo_path)
   end
 end
--- spec/controllers/repository_controller_spec.rb
+++ spec/controllers/repository_controller_spec.rb
@@ -6,11 +6,11 @@ describe RepositoryController do
 
     before { get :index }
 
-    it 'should return a redirect' do
+    it 'responds with redirect' do
       expect(response).to be_redirect
     end
 
-    it 'should redirect to root view' do
+    it 'redirects to root view' do
       expect(response).to redirect_to(root_url)
     end
   end
@@ -18,94 +18,67 @@ describe RepositoryController do
   context 'given a request with repo owner and name' do
 
     before do
-      HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).returns(repo)
+      RepositoryService.any_instance.stubs(:repo_with_last_commit).returns(repo)
       repo.stubs(:latest_commit).returns(commit)
     end
 
     let(:repo) do
-      HazCommitz::Repository.new('owner_name', 'repo_name')
+      Repository.new('owner_name', 'repo_name')
     end
 
     let(:commit) do
-      HazCommitz::Commit.new('sha-12345', 'joe bloggs', time_days_ago(7), 'i fixed stuff')
+      Commit.new('sha-12345', 'joe bloggs', time_days_ago(7), 'i fixed stuff')
     end
 
     context 'with valid repo' do
 
-      context 'to html url' do
+      context 'html request' do
 
-        before { get('/repos/owner_name/repo_name') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name' }
 
-        xit 'should request repo info' do
-          HazCommitz::RepositoryService.any_instance.expects(:repo_with_last_commit).with('owner_name', 'repo_name').returns(repo)
-          get('/repos/owner_name/repo_name')
+        it 'requests repository data' do
+          RepositoryService.any_instance.expects(:repo_with_last_commit).with('owner_name', 'repo_name').returns(repo)
+          get :show, user_id: 'owner_name', id: 'repo_name'
         end
 
-        xit 'should return valid response' do
-          expect(last_response).to be_ok
+        it 'returns ok response' do
+          expect(response).to be_ok
         end
 
-        xit 'should render repo view' do
-          expect(last_response.body).to have_selector('div#repo')
+        it 'renders repository view' do
+          expect(response).to render_template(:show)
         end
 
-        xit 'should display repo owner and name' do
-          expect(last_response.body).to contain(/owner_name/)
-          expect(last_response.body).to contain(/repo_name/)
-        end
-
-        xit 'should display rating' do
-          expect(last_response.body).to have_selector('span', content: "#{repo.rating}")
-        end
-
-        xit 'should display last commit info' do
-          expect(last_response.body).to contain(/sha-12345/)
-          expect(last_response.body).to contain(/joe bloggs/)
-          expect(last_response.body).to contain(/i fixed stuff/)
-        end
-
-        xit 'should display image with badge' do
-          expect(last_response.body).to have_xpath("//img[@src='/repos/owner_name/repo_name/badge.svg']")
-        end
-
-        context 'with a last commit that has no message' do
-
-          let(:commit) do
-            HazCommitz::Commit.new('sha-12345', 'joe bloggs', time_days_ago(7))
-          end
-
-          xit 'should not display any message or indication of' do
-            expect(last_response.body).not_to contain(/Message/)
-          end
+        it 'passes repository to view' do
+          expect(assigns(:repo)).to eq(repo)
         end
       end
 
-      context 'to badge url' do
+      context 'badge request' do
 
-        before { get('/repos/owner_name/repo_name/badge.svg') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name', format: :svg }
 
-        xit 'should request repo info' do
-          HazCommitz::RepositoryService.any_instance.expects(:repo_with_last_commit).with('owner_name', 'repo_name').returns(repo)
-          get('/repos/owner_name/repo_name/badge.svg')
+        it 'requests repository data' do
+          RepositoryService.any_instance.expects(:repo_with_last_commit).with('owner_name', 'repo_name').returns(repo)
+          get :show, user_id: 'owner_name', id: 'repo_name', format: :svg
         end
 
-        xit 'should return valid response' do
-          expect(last_response).to be_ok
+        it 'returns ok response' do
+          expect(response).to be_ok
         end
 
-        xit 'should return svg content type' do
-          expect(last_response.content_type).to include('image/svg+xml')
+        it 'responds with svg content type' do
+          expect(response.content_type).to include('image/svg+xml')
         end
 
         it 'should return an image'
 
-        xit 'should set etag in response headers' do
-          expect(last_response['Etag']).not_to be_nil
-          expect(last_response['Etag']).not_to be_empty
+        it 'should set etag in response headers' do
+          expect(response.etag).to be_present
         end
 
-        xit 'should set caching directive' do
-          expect(last_response['Cache-Control']).to eq('no-cache, no-store, must-revalidate')
+        it 'does not set caching directive' do
+          expect(response['Cache-Control']).to eq('no-cache')
         end
       end
     end
@@ -113,56 +86,59 @@ describe RepositoryController do
     context 'with a repo that does not exist' do
 
       before do
-        HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::NotFound)
+        RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::NotFound)
       end
 
-      context 'to html url' do
+      context 'html request' do
 
-        before { get('/repos/owner_name/repo_name') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name' }
 
-        xit 'should return not found status' do
-          expect(last_response.status).to eq(404)
+        it 'returns not found status' do
+          expect(response.status).to eq(404)
         end
       end
 
-      context 'to badge url' do
+      context 'badge request' do
 
-        before { get('/repos/owner_name/repo_name/badge.svg') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name', format: :svg }
 
-        xit 'should return not found status' do
-          expect(last_response.status).to eq(404)
+        it 'returns not found status' do
+          expect(response.status).to eq(404)
         end
 
-        it 'returns an error image'
+        it 'returns an error image' do
+          # will treat this as an image test until that is actually fixed
+          expect(response.content_type).to include('image/svg+xml')
+        end
       end
     end
 
     context 'with a repo that is unauthorised' do
 
       before do
-        HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::Unauthorized)
+        RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::Unauthorized)
       end
 
-      context 'to html url' do
+      context 'html request' do
 
-        before { get('/repos/owner_name/repo_name') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name' }
 
-        xit 'should return unauthorised status' do
-          expect(last_response.status).to eq(401)
+        it 'returns unauthorised status' do
+          expect(response.status).to eq(401)
         end
       end
 
-      context 'to badge url' do
+      context 'badge request' do
 
-        before { get('/repos/owner_name/repo_name/badge.svg') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name', format: :svg }
 
-        xit 'should return unauthorised status' do
-          expect(last_response.status).to eq(401)
+        it 'returns unauthorised status' do
+          expect(response.status).to eq(401)
         end
 
-        xit 'returns an error image' do
+        it 'returns an error image' do
           # will treat this as an image test until that is actually fixed
-          expect(last_response.content_type).to include('image/svg+xml')
+          expect(response.content_type).to include('image/svg+xml')
         end
       end
     end
@@ -170,29 +146,29 @@ describe RepositoryController do
     context 'when the GitHub API rate limit is exceeded' do
 
       before do
-        HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::TooManyRequests)
+        RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Octokit::TooManyRequests)
       end
 
-      context 'to html url' do
+      context 'html request' do
 
-        before { get('/repos/owner_name/repo_name') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name' }
 
-        xit 'should return service unavailble status' do
-          expect(last_response.status).to eq(503)
+        it 'returns service unavailble status' do
+          expect(response.status).to eq(503)
         end
       end
 
-      context 'to badge url' do
+      context 'badge request' do
 
-        before { get('/repos/owner_name/repo_name/badge.svg') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name', format: :svg }
 
-        xit 'should return service unavailble status' do
-          expect(last_response.status).to eq(503)
+        it 'returns service unavailble status' do
+          expect(response.status).to eq(503)
         end
 
-        xit 'returns an error image' do
+        it 'returns an error image' do
           # will treat this as an image test until that is actually fixed
-          expect(last_response.content_type).to include('image/svg+xml')
+          expect(response.content_type).to include('image/svg+xml')
         end
       end
     end
@@ -200,29 +176,29 @@ describe RepositoryController do
     context 'when an exception is thrown' do
 
       before do
-        HazCommitz::RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Exception)
+        RepositoryService.any_instance.stubs(:repo_with_last_commit).raises(Exception)
       end
 
-      context 'to html url' do
+      context 'html request' do
 
-        before { get('/repos/owner_name/repo_name') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name' }
 
-        xit 'should return service unavailble status' do
-          expect(last_response.status).to eq(500)
+        it 'returns service unavailble status' do
+          expect(response.status).to eq(500)
         end
       end
 
-      context 'to badge url' do
+      context 'badge request' do
 
-        before { get('/repos/owner_name/repo_name/badge.svg') }
+        before { get :show, user_id: 'owner_name', id: 'repo_name', format: :svg }
 
-        xit 'should return service unavailble status' do
-          expect(last_response.status).to eq(500)
+        it 'returns service unavailble status' do
+          expect(response.status).to eq(500)
         end
 
-        xit 'returns an error image' do
+        it 'returns an error image' do
           # will treat this as an image test until that is actually fixed
-          expect(last_response.content_type).to include('image/svg+xml')
+          expect(response.content_type).to include('image/svg+xml')
         end
       end
     end
@@ -232,11 +208,11 @@ describe RepositoryController do
 
     before { get :new }
 
-    it 'should return valid response' do
+    it 'returns valid response' do
       expect(response).to be_success
     end
 
-    it 'should render new view' do
+    it 'renders new view' do
       expect(response).to render_template('repository/new')
     end
 
@@ -261,12 +237,12 @@ describe RepositoryController do
         Commit.new('sha-12345', 'joe bloggs', time_days_ago(7), 'i fixed stuff')
       end
 
-      it 'should return redirect' do
+      it 'returns redirect' do
         expect(response).to be_redirect
       end
 
-      it 'should redirect to repo view' do
-        expect(response).to redirect_to action: :show, id: repo.path
+      it 'redirects to repo view' do
+        expect(response).to redirect_to action: :show, user_id: repo.owner, id: repo.name
 
         # not interested if the response is valid, but need to stub repo service to avoid hitting api
         expect(response).to redirect_to("/repos/#{repo.path}")
@@ -277,15 +253,15 @@ describe RepositoryController do
 
       before { post :create, repository: { path: 'blah' } }
 
-      it 'should redirect to /add' do
+      it 'returns redirect' do
         expect(response).to be_redirect
       end
 
-      it 'should redirect to new view' do
+      it 'redirects to new view' do
         expect(response).to redirect_to action: :new
       end
 
-      it 'should display flash message' do
+      it 'displays flash message' do
         expect(flash[:error]).to match(/Invalid repository format/)
       end
     end
