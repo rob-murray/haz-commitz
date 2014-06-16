95725932ccca558b4c0fda84fc15d72950b5714c
Rob Murray <robmurray17@gmail.com>
2014-06-16 19:52:48 +0100

    Fix logging errr

--- app/models/image_proxy.rb
+++ app/models/image_proxy.rb
@@ -2,11 +2,9 @@ require 'open-uri'
 
 class ImageProxy
   def self.fetch(url)
-    begin
       open(url).read
     rescue => e
-      Rails.error e
+      Rails.logger.warn e
       raise BadgeRequestError
-    end
   end
 end
