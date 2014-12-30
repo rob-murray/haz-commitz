26b90e1e51b7db1358944f22bedc50d87915f7c5
Rob Murray <robmurray17@gmail.com>
2014-12-30 17:32:09 +0000

    Ooops fix extra format param

--- app/presenters/badge_url_rating_presenter.rb
+++ app/presenters/badge_url_rating_presenter.rb
@@ -11,12 +11,12 @@ class BadgeUrlRatingPresenter
   def display
     status_colour_segment =  case @rating
         when 1 then '%3E%201%20year-red' # > 1 year
-        when 2..3 then '%3C%201%20year-orange.svg' # < 1 year
-        when 4..5 then '%3C%206%20months-yellow.svg' # < 6 months
-        when 6..7 then '%3C%203%20months-yellowgreen.svg' # < 3 months
-        when 8..9 then '%3C%201%20month-brightgreen.svg' # < 1 month
+        when 2..3 then '%3C%201%20year-orange' # < 1 year
+        when 4..5 then '%3C%206%20months-yellow' # < 6 months
+        when 6..7 then '%3C%203%20months-yellowgreen' # < 3 months
+        when 8..9 then '%3C%201%20month-brightgreen' # < 1 month
         when 10 then '%3C%201%20week-brightgreen' # < 1 week
-        else 'None-red.svg'
+        else 'None-red'
         end
 
     [BASE_URI, SUBJECT, SEPARATOR, status_colour_segment, FORMAT].join
