b9d1a1dfcc9eab9d2b18ebe91b92a82b5f7b5889
Rob Murray <robmurray17@gmail.com>
2015-01-11 18:47:52 +0000

    Just use values gem here

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,10 +1,3 @@
-class Commit
-  attr_reader :sha, :author_name, :date, :message
+class Commit < Value.new(:sha, :author_name, :date, :message)
 
-  def initialize(sha, author_name, date, message = '')
-    @sha = sha
-    @author_name = author_name
-    @date = date
-    @message = message
-  end
 end
